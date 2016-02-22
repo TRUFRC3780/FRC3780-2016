6289302bea384fd3cc1f1f761fb7efb21e071bfa
LuminousRubyist <andytaelee@gmail.com>
2016-02-22 10:07:10 -0500

    Init variables in Arm subsystem

--- src/org/usfirst/frc/team3780/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team3780/robot/subsystems/Arm.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3780.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
 
 /**
  *
@@ -10,6 +11,8 @@ public class Arm extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	private Talon joint1, joint2;
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
