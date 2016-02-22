0b0c882b0edbaf484bde28419cce4e91385cd339
LuminousRubyist <andytaelee@gmail.com>
2016-02-22 10:05:46 -0500

    Init subsystems in Robot.java, comment out Chooser, LiveWindow

--- src/org/usfirst/frc/team3780/robot/Robot.java
+++ src/org/usfirst/frc/team3780/robot/Robot.java
@@ -1,14 +1,10 @@
 
 package org.usfirst.frc.team3780.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team3780.robot.commands.ExampleCommand;
-import org.usfirst.frc.team3780.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.*;
+import org.usfirst.frc.team3780.robot.subsystems.*;
+import org.usfirst.frc.team3780.robot.commands.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,11 +15,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
+	public static Chassis chassis;
+	public static
+	
 
     Command autonomousCommand;
-    SendableChooser chooser;
+//    SendableChooser chooser;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -31,10 +29,10 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
+//        chooser = new SendableChooser();
+//        chooser.addDefault("Default Auto", new ExampleCommand());
+////        chooser.addObject("My Auto", new MyAutoCommand());
+//        SmartDashboard.putData("Auto mode", chooser);
     }
 	
 	/**
@@ -60,7 +58,7 @@ public class Robot extends IterativeRobot {
 	 * or additional comparisons to the switch structure below with additional strings & commands.
 	 */
     public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
+//        autonomousCommand = (Command) chooser.getSelected();
         
 		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
 		switch(autoSelected) {
@@ -103,6 +101,6 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-        LiveWindow.run();
+//        LiveWindow.run();
     }
 }
