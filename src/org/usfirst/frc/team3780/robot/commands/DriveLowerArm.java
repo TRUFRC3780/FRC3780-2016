ea20c09e60a7773ec8810bc344da4fbce10c8679
LuminousRubyist <andytaelee@gmail.com>
2016-02-25 14:47:54 -0500

    Flesh out DriveLowerCommand

--- src/org/usfirst/frc/team3780/robot/commands/DriveLowerArm.java
+++ src/org/usfirst/frc/team3780/robot/commands/DriveLowerArm.java
@@ -1,23 +1,35 @@
 package org.usfirst.frc.team3780.robot.commands;
 
+import org.usfirst.frc.team3780.robot.*;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class DriveLowerArm extends Command {
-
+	
+	private Joystick joystick = Robot.oi.getArmJoystick();
+	
     public DriveLowerArm() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	requires(Robot.arm);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.arm.stopLower();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double y = joystick.getY();
+    	if(y > 0) {
+    		Robot.arm.driveLower(1.0);
+    	} else if(y < 0) {
+    		Robot.arm.driveLower(-1.0);
+    	} else { // y == 0
+    		Robot.arm.stopLower();
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -27,10 +39,12 @@ public class DriveLowerArm extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.arm.stopLower();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
