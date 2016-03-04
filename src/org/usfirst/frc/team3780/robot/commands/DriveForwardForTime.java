f863afc67e0675476a852c3b6057271bc7e1bef0
LuminousRubyist <andytaelee@gmail.com>
2016-03-04 18:53:30 -0500

    DriveForwardForTime command

---
+++ src/org/usfirst/frc/team3780/robot/commands/DriveForwardForTime.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team3780.robot.commands;
+
+import org.usfirst.frc.team3780.robot.*;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author Andrew Lee
+ *
+ */
+public class DriveForwardForTime extends Command {
+
+	private Timer timer;
+	private double endTime;
+	private double speed;
+	
+    public DriveForwardForTime(double seconds, double speed) {
+    	requires(Robot.chassis);
+    	timer = new Timer();
+    	endTime = seconds;
+    	this.speed = speed;
+    	
+    	timer.stop();
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.chassis.driveForward(speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return timer.get() >= endTime;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.chassis.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
