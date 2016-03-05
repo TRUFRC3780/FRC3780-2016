1c2c4f204fae57f68fbc6a577d862db4bcc79bc4
LuminousRubyist <andytaelee@gmail.com>
2016-03-04 19:00:43 -0500

    Forward and backward buttons

--- src/org/usfirst/frc/team3780/robot/RobotMap.java
+++ src/org/usfirst/frc/team3780/robot/RobotMap.java
@@ -35,6 +35,8 @@ public class RobotMap {
     public static final int LEFT_JOYSTICK_ID = 1;
     public static final int RIGHT_JOYSTICK_ID = 1;
     
-    // Compressor PCM # --> placeholder
-    public static final int COMPRESSOR_PCM_ID = 1;
+    // Upper arm forward backward buttons
+    public static final int UPPER_ARM_FORWARD_BUTTON_ID  = 3;
+    public static final int UPPER_ARM_BACKWARD_BUTTON_ID = 2;
+    
 }
