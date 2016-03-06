d197be6c508dca0ddc26704a789fd9e98195c6c8
Hippo Whisperer <Hippo Whisperer>
2016-03-06 15:18:08 -0500

    Scylla (arm) subsystem

--- src/org/usfirst/frc/team3780/robot/subsystems/Scylla.java
+++ src/org/usfirst/frc/team3780/robot/subsystems/Scylla.java
@@ -2,11 +2,41 @@ package org.usfirst.frc.team3780.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import org.usfirst.frc.team3780.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.*;
+
 /**
- *
+ * @author Andrew Lee
+ * 
  */
 public class Scylla extends Subsystem {
     
+	private Talon leftTalon ,rightTalon;
+	
+	public Scylla() {
+		leftTalon  = new Talon(RobotMap.SCYLLA_TALON_LEFT_ID);
+		rightTalon = new Talon(RobotMap.SCYLLA_TALON_RIGHT_ID);
+	}
+	
+	public void drive(double d) {
+		leftTalon.set(d);
+		rightTalon.set(-1 * d);
+	}
+	
+	public void driveForward(double d) {
+		drive(d);
+	}
+	
+	public void driveBackward(double d) {
+		drive(-1 * d);
+	}
+	
+	public void stop() {
+		leftTalon.set(0.0);
+		rightTalon.set(0.0);
+	}
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
