90b1a187f8b8b10c5a01c202149023c52f05bbbc
Andrew Lee <andytaelee@gmail.com>
2016-09-13 19:57:36 -0400

    September 2016 robot demo port updates

--- src/org/usfirst/frc/team3780/robot/RobotMap.java
+++ src/org/usfirst/frc/team3780/robot/RobotMap.java
@@ -4,7 +4,7 @@ package org.usfirst.frc.team3780.robot;
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
- * 
+ *
  * @author Andrew Lee
  */
 public class RobotMap {
@@ -12,18 +12,18 @@ public class RobotMap {
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
-    
+
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
-    
+
     // Drive Train
-    public static final int DRIVE_FRONT_LEFT  = 3;
-    public static final int DRIVE_REAR_LEFT   = 5;
-    public static final int DRIVE_FRONT_RIGHT = 1;
-    public static final int DRIVE_REAR_RIGHT  = 2;
-    
+    public static final int DRIVE_FRONT_LEFT  = 0;
+    public static final int DRIVE_REAR_LEFT   = 1;
+    public static final int DRIVE_FRONT_RIGHT = 2;
+    public static final int DRIVE_REAR_RIGHT  = 3;
+
     // Scylla Talon numbers
     public static final int SCYLLA_TALON_RIGHT_ID = 0;
     public static final int SCYLLA_TALON_LEFT_ID  = 4;
@@ -31,7 +31,7 @@ public class RobotMap {
     // Drive Joystick port
     public static final int LEFT_JOYSTICK_ID  = 1; // Telemachus
     public static final int RIGHT_JOYSTICK_ID = 0; // Odysseus
-    
+
     // Scylla buttons
     public static final int SCYLLA_FORWARD_BUTTON_ID        = 3;
     public static final int SCYLLA_BACKWARD_BUTTON_ID       = 2;
